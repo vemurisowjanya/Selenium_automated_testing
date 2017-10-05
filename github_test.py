9e60a27ad77a5dd18b9a37beee9775cf3838c5e1
vemurisowjanya <sowjanyavemuri1997@gmail.com>
2017-10-06 01:45:52 +0530

    Add files via upload

---
+++ github_test.py
@@ -0,0 +1,79 @@
+import time
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class GithubTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.base_url = "https://github.com"
+
+
+    def test_github_signup(self):
+        driver = self.driver
+        driver.get(self.base_url)
+        search_box = driver.find_element_by_link_text("Sign up")
+        search_box.send_keys(Keys.RETURN)
+        time.sleep(2)
+        assert "Join GitHub" in driver.page_source
+
+
+    def test_github_fork_and_star_repo_annonymous_user_redirects_to_login(self):
+        login_url = "https://github.com/login?return_to=%2Fsysters%2Fmailman3"
+        driver = self.driver
+        driver.get("{0}/systers/mailman3".format(self.base_url))
+        elem = driver.find_element_by_partial_link_text("Fork")
+        elem.send_keys(Keys.ENTER)
+        time.sleep(2)
+        assert login_url == driver.current_url
+
+        driver.get("{0}/systers/mailman3".format(self.base_url))
+        elem = driver.find_element_by_partial_link_text("Star")
+        elem.send_keys(Keys.ENTER)
+        time.sleep(2)
+        assert login_url == driver.current_url
+
+
+    def test_github_explore(self):
+        python_trending_url = "https://github.com/trending/python"
+        driver = self.driver
+        driver.get(self.base_url)
+        search_box = driver.find_element_by_link_text("Explore")
+        search_box.send_keys(Keys.RETURN)
+        time.sleep(2)
+        trending_elem = driver.find_element_by_partial_link_text("Trending")
+        trending_elem.send_keys(Keys.ENTER)
+        time.sleep(2)
+        python_lang = driver.find_element_by_link_text("Python")
+        python_lang.send_keys(Keys.ENTER)
+        time.sleep(2)
+        assert python_trending_url == driver.current_url
+
+
+    def test_github_systers_automated_testing(self):
+        automated_testing_url = self.base_url + "/systers/automated-testing"
+        readme_url = automated_testing_url + "/blob/master/README.md"
+        driver = self.driver
+        driver.get("{0}/systers/automated-testing".format(self.base_url))
+        wiki_elem = driver.find_element_by_partial_link_text("webdriveriki")
+        wiki_elem.send_keys(Keys.ENTER)
+        time.sleep(2)
+        readme_elem = driver.find_element_by_link_text("Setup Steps")
+        readme_elem.send_keys(Keys.ENTER)
+        time.sleep(2)
+        assert driver.current_url == readme_url
+
+
+    def test_github_repo_search_with_invalid_string(self):
+        driver = self.driver
+        driver.get(self.base_url)
+        search_box = driver.find_element_by_name("q")
+        search_box.send_keys("**************")
+        search_box.send_keys(Keys.RETURN)
+        time.sleep(2)
+        assert "search help section"  in driver.page_source
+
+
+    def tearDown(self):
+        self.driver.close()
